ef897d5e0fed5aa113d53f5a83941b181601364c
nick <nbsullivan@gmail.com>
2015-04-27 16:07:47 -0700

    more comments

--- mongodbprepandstreetclean.py
+++ mongodbprepandstreetclean.py
@@ -6,13 +6,14 @@ import re
 import codecs
 import json
 
-
+#re stuff for potientally bad names
 lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
 CREATED = [ 'version', 'changeset', 'timestamp', 'user', 'uid']
 
+#build this up with auditstreetnames.py
 mapping = { "St": "Street",
             "St.": "Street",
             "Ave" : "Avenue",
@@ -35,30 +36,38 @@ mapping = { "St": "Street",
             "street" : "Street"
             }
 
-
+#shape the elements. 
 def shape_element(element):
     node = {}
     if element.tag == "node" or element.tag == "way" :
         node = element.attrib
+        #make sure the node isnt empty
         if node == {}:
             element.clear()
             return None
+        
+        #start building up the keys of the node
         node['type'] = element.tag
         node['created'] = {}
         node['pos'] = []
+        #grab up the data from fields in created
         for each in CREATED:
             if each in node.keys():
                 node['created'][each] = element.attrib[each]
                 del node[each]
+        #translate the lat and lon as a array
         if 'lat' in node.keys() and 'lon' in node.keys():
             node['pos'].append(float(node['lat']))
             node['pos'].append(float(node['lon']))
             del node['lat']
             del node['lon']
+        #make the address subdict
         node['address'] = {}
+        #if it is a way get a node_refs array
         if node['type'] == 'way':
             node['node_refs'] = []
         for child in element:
+            #loop over the subtags and put in the apporiate node_refs or address data            
             if child.tag == 'nd':
                 node['node_refs'].append(child.attrib['ref'])
                 
@@ -69,6 +78,7 @@ def shape_element(element):
                     else:
                         node[child.attrib['k']] = child.attrib['v']
 
+        #now the fun street name replacement 
         if node['address'] != {}:
             if node['address'].has_key('street'):
                 for key in mapping.keys():
@@ -76,19 +86,25 @@ def shape_element(element):
                         newaddress = replace_last(node['address']['street'], key, mapping[key])
                         node['address']['street'] = newaddress
                         break
+        #if an address was not found remove the field
         if node['address'] == {}:
             del node['address']
         return node
+    #if its not a tag or a way return nothing    
     else:
         return None
 
+
+#for replacing street names
 def replace_last(source, old, new):
     head, sep, tail = source.rpartition(old)
     return head + new
 
+
 def process_map(file_in, pretty = False):
     file_out = "{0}.json".format(file_in)
     data = []
+    #write out the convereted osm to a json, probably should not be storing the whole thing in the data varible, this is probably why I am leaking so much memory
     with codecs.open(file_out, "w") as fo:
         for event, element in ET.iterparse(file_in, events = ("start", "end")):
             if event == 'end':
